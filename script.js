ab065051ddcecdf2b6dd0f7051314dc4cac0e32a
Brenndan Croteau <brenndancroteau@gmail.com>
2024-08-02 19:33:27 -0500

    Website fully migrated

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ function doalert(checkboxElem) {
     let vidSource = document.getElementById("vidSource")
     let style = document.getElementById("styles")
     if (checkboxElem.checked) {
-        style.href = "styles/styleNature.css"
-        vidSource.src = "mp4/nature.mp4"
+        style.href = "../styles/styleNature.css"
+        vidSource.src = "../mp4/nature.mp4"
         vid.load()
         vid.play()
         localStorage.setItem('backgroundChoice', 'nature')
     }
     else {
-        style.href = "styles/styleSpace.css"
-        vidSource.src = "mp4/sky.mp4"
+        style.href = "../styles/styleSpace.css"
+        vidSource.src = "../mp4/sky.mp4"
         vid.load()
         vid.play()
         localStorage.setItem('backgroundChoice', 'sky');
